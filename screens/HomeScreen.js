f0c070a8723c81b48152b5a6e7bc75c92c9b426d
Hartz-byte <harshmail281199@gmail.com>
2024-02-02 13:29:34 +0530

    home top section

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,35 @@
-import { Text, View } from "react-native";
+import { Text, View, Platform, TouchableOpacity } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import { styles } from "../theme";
+
+const platform = Platform.OS == "android";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View className="flex-1 bg-neutral-800">
+      {/* top head section */}
+      <SafeAreaView className={platform ? "mb-3" : "-mb-2"}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between items-center mx4">
+          {/* options icon */}
+          <Bars3CenterLeftIcon size="30" strokeWidth={2} color="white" />
+          {/* "movies" text */}
+          <Text className="text-white text-3xl font-bold">
+            <Text style={styles.text}>M</Text>ovies
+          </Text>
+          {/* search icon */}
+          <TouchableOpacity>
+            <MagnifyingGlassIcon size={30} strokeWidth={2} color="white" />
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+    </View>
   );
 };
 
