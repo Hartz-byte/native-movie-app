12ef70c299c1291ab2c2f2b60a2049f82eca2a14
Hartz-byte <harshmail281199@gmail.com>
2024-02-05 12:37:44 +0530

    movie screen update

--- components/Cast.js
+++ components/Cast.js
@@ -1,12 +1,41 @@
-import { Text, View } from 'react-native'
-import React from 'react'
+import { Text, View, ScrollView, TouchableOpacity } from "react-native";
+import React from "react";
 
-const Cast = () => {
+const Cast = ({ cast }) => {
+  let personName = "Keanu Reevs";
+  let characterName = "John Wick";
+
   return (
-    <View>
-      <Text>Cast</Text>
-    </View>
-  )
-}
+    <View className="mx-6">
+      <Text className="text-white text-lg mx-4 mb-5">Top Cast</Text>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ padding: 15 }}
+      >
+        {cast &&
+          cast.map((person, index) => {
+            return (
+              <TouchableOpacity key={index} className="mr-4 items-center">
+                {/* character name */}
+                <Text className="text-white text-xs mt-1">
+                  {characterName.length > 10
+                    ? characterName.slice(0, 10) + "..."
+                    : characterName}
+                </Text>
 
-export default Cast
+                {/* person name */}
+                <Text className="text-neutral text-xs mt-1">
+                  {personName.length > 10
+                    ? personName.slice(0, 10) + "..."
+                    : personName}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+      </ScrollView>
+    </View>
+  );
+};
+
+export default Cast;
