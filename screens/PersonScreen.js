fbc7c40d261965c7f4abe4e0f92425efcc790138
Hartz-byte <harshmail281199@gmail.com>
2024-02-05 20:08:37 +0530

    person screen update

--- screens/PersonScreen.js
+++ screens/PersonScreen.js
@@ -1,14 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import {
+  Dimensions,
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  SafeAreaView,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { useNavigation } from "@react-navigation/native";
+import { ChevronLeftIcon } from "react-native-heroicons/outline";
+import { HeartIcon } from "react-native-heroicons/solid";
+import { styles } from "../theme";
+
+var { width, height } = Dimensions.get("window");
 
 const PersonScreen = () => {
+  const navigation = useNavigation();
+  const [isFavorite, setIsFavorite] = useState(false);
+
   return (
-    <View>
-      <Text>PersonScreen</Text>
-    </View>
-  )
-}
+    <ScrollView
+      className="flex-1 bg-neutral-900"
+      contentContainerStyle={{ paddingBottom: 20 }}
+    >
+      <SafeAreaView
+        className={
+          "z-20 w-full flex-row justify-between items-center px-4 my-20"
+        }
+      >
+        {/* back btn */}
+        <TouchableOpacity
+          style={styles.background}
+          className="rounded-xl p-1"
+          onPress={() => navigation.goBack()}
+        >
+          <ChevronLeftIcon size="28" strokeWidth={2.5} color="white" />
+        </TouchableOpacity>
 
-export default PersonScreen
+        {/* heart icon */}
+        <TouchableOpacity onPress={() => setIsFavorite(!isFavorite)}>
+          <HeartIcon size="35" color={isFavorite ? "red" : "white"} />
+        </TouchableOpacity>
+      </SafeAreaView>
+    </ScrollView>
+  );
+};
 
-const styles = StyleSheet.create({})
+export default PersonScreen;
