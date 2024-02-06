40eb8560528cf8a44d2dfef750b71140a180f845
Hartz-byte <harshmail281199@gmail.com>
2024-02-06 12:19:19 +0530

    api call

--- components/MovieCard.js
+++ components/MovieCard.js
@@ -1,17 +1,18 @@
-import { TouchableOpacity, Dimensions, Image } from "react-native";
+import { TouchableWithoutFeedback, Dimensions, Image } from "react-native";
 import React from "react";
+import { image500 } from "../api";
 
 var { width, height } = Dimensions.get("window");
 
 const MovieCard = ({ item, handleClick }) => {
   return (
-    <TouchableOpacity onPress={() => handleClick(item)}>
+    <TouchableWithoutFeedback onPress={() => handleClick(item)}>
       <Image
         source={require("../assets/images/moviePoster1.png")}
         style={{ width: width * 0.6, height: height * 0.4 }}
         className="rounded-3xl"
       />
-    </TouchableOpacity>
+    </TouchableWithoutFeedback>
   );
 };
 
---
+++ constants/index.js
@@ -0,0 +1,1 @@
+export const apiKey = "5bb321af5b99e4f0046180e8105fabe2";
