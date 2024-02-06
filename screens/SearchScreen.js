283e0a52b7d7f9bf09722d0944765d2e988dbce2
Hartz-byte <harshmail281199@gmail.com>
2024-02-06 14:00:01 +0530

    completed

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,37 +1,58 @@
 import {
-  Dimensions,
-  Text,
   View,
+  Text,
   TextInput,
   TouchableOpacity,
+  Image,
   ScrollView,
   TouchableWithoutFeedback,
-  Image,
+  Dimensions,
 } from "react-native";
-import React, { useState } from "react";
-import { useNavigation } from "@react-navigation/native";
+import React, { useCallback, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { XMarkIcon } from "react-native-heroicons/outline";
+import { useNavigation } from "@react-navigation/native";
+import { fallbackMoviePoster, image185, searchMovies } from "../api";
+import { debounce } from "lodash";
 import Loading from "../components/Loading";
 
-var { width, height } = Dimensions.get("window");
-const movieName = "Avengers: End Game";
+const { width, height } = Dimensions.get("window");
 
-const SearchScreen = () => {
+export default function SearchScreen() {
   const navigation = useNavigation();
-  const [results, setResult] = useState([1, 2, 3, 4, 5]);
   const [loading, setLoading] = useState(false);
+  const [results, setResults] = useState([]);
+
+  const handleSearch = (search) => {
+    if (search && search.length > 2) {
+      setLoading(true);
+      searchMovies({
+        query: search,
+        include_adult: false,
+        language: "en-US",
+        page: "1",
+      }).then((data) => {
+        setLoading(false);
+        if (data && data.results) setResults(data.results);
+      });
+    } else {
+      setLoading(false);
+      setResults([]);
+    }
+  };
+
+  const handleTextDebounce = useCallback(debounce(handleSearch, 400), []);
 
   return (
     <SafeAreaView className="bg-neutral-800 flex-1">
+      {/* search input */}
       <View className="mx-4 mb-3 flex-row justify-between items-center border border-neutral-500 rounded-full">
-        {/* seach text input */}
         <TextInput
+          onChangeText={handleTextDebounce}
           placeholder="Search Movie"
           placeholderTextColor={"lightgray"}
           className="pb-1 pl-6 flex-1 text-base font-semibold text-white tracking-wider"
         />
-        {/* back to home btn */}
         <TouchableOpacity
           onPress={() => navigation.navigate("Home")}
           className="rounded-full p-3 m-1 bg-neutral-500"
@@ -40,21 +61,18 @@ const SearchScreen = () => {
         </TouchableOpacity>
       </View>
 
-      {/* loading check */}
+      {/* search results */}
       {loading ? (
         <Loading />
       ) : results.length > 0 ? (
         <ScrollView
-          showsHorizontalScrollIndicator={false}
+          showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingHorizontal: 15 }}
           className="space-y-3"
         >
-          {/* results count */}
           <Text className="text-white font-semibold ml-1">
             Results ({results.length})
           </Text>
-
-          {/* results view */}
           <View className="flex-row justify-between flex-wrap">
             {results.map((item, index) => {
               return (
@@ -64,14 +82,17 @@ const SearchScreen = () => {
                 >
                   <View className="space-y-2 mb-4">
                     <Image
+                      source={{
+                        uri: image185(item.poster_path) || fallbackMoviePoster,
+                      }}
+                      // source={require('../assets/images/moviePoster2.png')}
                       className="rounded-3xl"
-                      source={require("../assets/images/moviePoster2.png")}
                       style={{ width: width * 0.44, height: height * 0.3 }}
                     />
-                    <Text className="text-neutral-300 ml-1">
-                      {movieName.length > 22
-                        ? movieName.slice(0, 22) + "..."
-                        : movieName}
+                    <Text className="text-gray-300 ml-1">
+                      {item.title.length > 22
+                        ? item.title.slice(0, 22) + "..."
+                        : item.title}
                     </Text>
                   </View>
                 </TouchableWithoutFeedback>
@@ -87,10 +108,6 @@ const SearchScreen = () => {
           />
         </View>
       )}
-
-      {/* search results */}
     </SafeAreaView>
   );
-};
-
-export default SearchScreen;
+}
